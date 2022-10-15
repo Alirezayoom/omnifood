9bccc447649815165e419ff1cafe3a1ab9be5a62
Alireza <alirezayoom@gmail.com>
2022-10-15 16:57:14 +0430

    sticky navigation bar added

--- js/script.js
+++ js/script.js
@@ -6,6 +6,30 @@ btnNavEl.addEventListener("click", function () {
 });
 
 ////////////////////////////////////////////////////
+// sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
+
+////////////////////////////////////////////////////
 //all links
 
 const allLinks = document.querySelectorAll("a:link");
